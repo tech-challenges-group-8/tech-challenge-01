4c209c3a7dc497837f1e347da1ea227019a217cc
Fernando Gustavo Cortez <132622525+FernandoGustavoCortez@users.noreply.github.com>
2025-05-27 18:44:22 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,40 +7,40 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import theme from "./styles/theme";
-import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
 import Investiments from "./pages/Investiments";
 import Services from "./pages/Services";
 import "./commons/i18n";
+import HeaderHome from "./components/home/HeaderHome";
+import FooterHome from "./components/home/FooterHome";
+import BodyHome from "./components/home/BodyHome";
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    // Prevents rendering on the server
-    return null;
-  }
+  
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Header />
-        <Box sx={{ display: "flex", height: "calc(100vh - 68px)" }}>
-          <Sidebar />
-          <Box sx={{ flex: 1, overflow: "auto", padding: 2 }}>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/transactions" element={<Transactions />} />
-              <Route path="/investiments" element={<Investiments />} />
-              <Route path="/services" element={<Services />} />
-            </Routes>
-          </Box>
-        </Box>
-      </BrowserRouter>
+      <HeaderHome />
+      <BodyHome />
+      <FooterHome />
     </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <CssBaseline />
+    //   <BrowserRouter>
+    //     <HeaderHome />
+    //     <FooterHome />
+    //     <Box sx={{ display: "flex", height: "calc(100vh - 68px)" }}>
+    //       <Sidebar />
+    //       <Box sx={{ flex: 1, overflow: "auto", padding: 2 }}>
+    //         <Routes>
+    //           <Route path="/" element={<Dashboard />} />
+    //           <Route path="/transactions" element={<Transactions />} />
+    //           <Route path="/investiments" element={<Investiments />} />
+    //           <Route path="/services" element={<Services />} />
+    //         </Routes>
+    //       </Box>
+    //     </Box>
+    //   </BrowserRouter>
+    // </ThemeProvider>
   );
 }
